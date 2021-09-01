119062b4dfac8f569ed5539ad36bc7061d0a9d6f
keentan <85555666+keentan@users.noreply.github.com>
2021-09-01 22:19:36 +0800

    Create async1.js

---
+++ async1.js
@@ -0,0 +1,9 @@
+// show the word hello
+console.log("Hello");
+// wait for 2 seconds, after 2 seconds show good bye (function inside a function - parameter)
+setTimeout(function() {
+	console.log("Goodbye");
+},2000);
+// White waiting continue the bottom first
+// show hello again
+console.log("Hello Again")
