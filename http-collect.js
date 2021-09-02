f5d3bd6257232c53cc996516a031e23081e26289
keentan <85555666+keentan@users.noreply.github.com>
2021-09-02 22:14:09 +0800

    Add files via upload

---
+++ http-collect.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+http.get(process.argv[2],function(response) {
+	var answer = "";
+	response.on("data",function(data) {
+		answer += data.toString();
+	});
+	response.on("error", function(err) {
+
+	})
+	response.on("end", function(data) {
+		console.log(answer.length);
+		console.log(answer);
+	})
+})
---
+++ new-server.js
@@ -0,0 +1,28 @@
+var http = require('http'), fs = require('fs');
+
+function serveStaticFile(res,path,contentType,responseCode) {
+	if(!responseCode) responseCode = 200;
+	fs.readFile(__dirname + path, function(err,data) {
+		if(err) {
+			res.writeHead(500, {'Content-Type':'text/plain'});
+			res.end('500 - Internal Error');
+		}
+		else {
+			res.writeHead(responseCode,
+				{'Content-Type':contentType});
+			res.end(data);
+		}
+	});
+}
+http.createServer(function(req,res) {
+	path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase();
+	switch(path) {
+		case '':serveStaticFile(res,'/public/html/home.html','text/html');
+		break;
+		case '/about': serveStaticFile(res, '/public/html/about.html','text/html');
+		break; case '/img/logo.jpg':serveStaticFile(res,'/public/img/logo.jpg','image/jpeg');
+		break;
+		default:serveStaticFile(res,'/public/html/404.html','text/html',404); break; 
+	}
+}).listen(3000);
+console.log('Server started on localhost:3000; press Ctrl-C to terminate...');
